ac73b59319a1eb90ea5a0161d408545d902a8bd4
GladysKigen <gladys.kigen@student.moringa.com>
2023-01-14 16:36:15 +0300

    Completed trials

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -6,11 +6,18 @@ function SpicyFoodList() {
 
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
-    console.log(newFood);
+    //console.log(newFood);
+    const newFoodArray = [...foods, newFood];
+  setFoods(newFoodArray);
+  }
+
+  function handleLiClick(id){
+    const newFoodArray = foods.filter((food) => food.id !== id);
+    setFoods(newFoodArray);
   }
 
   const foodList = foods.map((food) => (
-    <li key={food.id}>
+    <li key={food.id} onClick={() => handleLiClick(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
@@ -22,5 +29,7 @@ function SpicyFoodList() {
     </div>
   );
 }
+
+
 
 export default SpicyFoodList;
